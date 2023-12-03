fdc99fe72b253e4cf3477da5aa15be67f5e8befa
Willian Almeida <willianac01@gmail.com>
2023-12-03 19:16:43 -0300

    feat: server is now emiting the last message both to sender and receiver

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -8,14 +8,17 @@ import {
 	ConnectedSocket,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { MessageService } from 'src/services/message.service';
 
 @WebSocketGateway({ cors: true })
 export class ChatGateway implements OnGatewayDisconnect {
+	constructor(private messageService: MessageService) {}
+
 	@WebSocketServer()
 	server: Server;
 
-	handleConnection(client: any) {
-		
+	handleConnection(client: Socket) {
+		console.log('conectou')
 	}
 
 	handleDisconnect(client: any) {
@@ -24,11 +27,20 @@ export class ChatGateway implements OnGatewayDisconnect {
 
 	@SubscribeMessage('message')
 	handleMessage(
-		@MessageBody() data: string,
+		@MessageBody() data: { receiverId: string; message: string },
 		@ConnectedSocket() client: Socket,
 	) {
-		console.log(data);
-		this.server.emit("message", "eu, server, recebi isso:" + data);
-		return data
+		this.messageService.postMessage({
+			senderId: client.handshake.auth.userid,
+			receiverId: data.receiverId,
+			text: data.message,
+		});
+
+		this.server.emit('message', {
+			senderId: client.handshake.auth.userid,
+			receiverId: data.receiverId,
+			text: data.message,
+		});
+		return data;
 	}
 }
